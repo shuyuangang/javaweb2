d1714bb90210f5b842d246e384980a89bdedf1e3
shuyuangang <1148316973@qq.com>
2020-04-11 21:14:32 +0800

    Initial commit

---
+++ src/org/example/HelloServlet.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by 11483 on 2020/4/3 22:26
+ */
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+        resp.getWriter().println("hello servlet!!!!!!!");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
